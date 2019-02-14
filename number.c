0c9f54ec5cdb0a327c331b7393821be0d4babdd1
sanjeethsanju <47620776+sanjeethsanju@users.noreply.github.com>
2019-02-14 10:33:41 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    if(a<0)
+    {
+    printf("negative");
+    }
+    else if(a>0)
+    {
+    printf("positive");
+    }
+    else
+    {
+    printf("zero");
+    }
+
+    return 0;
+}
